0723d25efb0a61bfc09ad612dcddb2ed07393308
Cozloschi <cozloschi.florin@gmail.com>
2019-11-06 20:32:01 +0200

    add bounce logic for projectile

--- src/utils/Systems/GameLoop.js
+++ src/utils/Systems/GameLoop.js
@@ -6,17 +6,17 @@ const GameLoop = (entities, { touches, time }) => {
     let spaceship = entities.spaceship
     let missle = entities.missle
 
-   
+    Matter.Body.setPosition(missle.body, Matter.Vector.create(spaceship.body.position.x, spaceship.body.position.y - 30))
+
     touches.filter(t => t.type === "move").forEach(t => {
-        /*if(Matter.Bounds.contains(spaceship.body.bounds, Matter.Vector.create(t.event.locationX, t.event.locationY))){
-            alert("CLICKED HIM")
-        }*/
-        if (t.event.locationX > spaceship.body.position.x){
+        //console.log(t.event.locationX)
+        if (t.event.locationX > 300) {
+            Matter.Body.applyForce(missle.body, missle.body.position, { x: 0, y: -2 })
+        }
+
+        if (t.event.locationX > spaceship.body.position.x) {
             Matter.Body.applyForce(spaceship.body, spaceship.body.position, { x: 0.5, y: 0.00 })
-            Matter.Body.applyForce(missle.body, missle.body.position, { x: 0, y: -5 })
-        }else{ //missle follows the spaceship
-            Matter.Body.setPosition(missle.body, Matter.Vector.create(spaceship.body.position.x, spaceship.body.position.y - 30))
-          
+        } else { //missle follows the spaceship
             Matter.Body.applyForce(spaceship.body, spaceship.body.position, { x: -0.5, y: 0.00 })
         }
     })
